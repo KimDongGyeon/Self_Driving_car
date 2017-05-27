ff4a8889b2a499899626f69c028f7abd7bd32d75
youngbin223 <youngbin4839@gmail.com>
2017-05-27 20:32:10 +0900

    Initial File to transform
    Get ready to code

--- Self_Driving_Car.ino
+++ Self_Driving_Car.ino
@@ -1,6 +1,6 @@
-int echo = 8, trig = 9;
-int ADir = 4, BDir = 7;
-int APWM = 5, BPWM = 6;
+int echo = 8, trig = 9; // 초음파 센서
+int ADir = 4, BDir = 7; // 모터 회전 방향
+int APWM = 5, BPWM = 6; // 모터 회전 속도
 
 void setup() {
   pinMode(echo,INPUT);
@@ -11,35 +11,25 @@ void setup() {
 }
 
 void loop() {
-  int dist = ultrasonic(echo,trig);
-  int speed = 255;
-  
-  Serial.println(dist);
+  int d = ultrasonic(echo,trig);
+  Serial.println(d);
 
-  if(dist>30) {Forward(speed);}
+  if(d>30) {Forward(255);}
   
-  else if(30>=dist>25) {
-    for(;dist=26;){
-      Forward(speed);
-      speed-=5;
-    }
+  else if(30>=d>29) {Forward(250);}
+  else if(29>=d>28) {Forward(245);}
+  else if(28>=d>27) {Forward(240);}
+  else if(27>=d>26) {Forward(235);}
+  else if(26>=d>25) {Forward(200);}
+  else if(20>d) {Backward(255);}
+  else if(21>=d>22) {Backward(255);}
+  else if(22>=d>23) {Backward(250);}
+  else if(23>=d>24) {Backward(249);}
+  else if(24>=d) {Backward(245);}
+  else {Forward(0);}
   }
 
-  else if(dist<20) {Backward(speed);}
-
-  else if(20<=dist<25) {
-    for(;dist=24;)
-    {
-      Backward(speed);
-      speed-=3;
-    }
-  }
-  
-  else{Forward(0);}
-  
-}
-
-int ultrasonic(int echo, int trig)
+int ultrasonic(int echo, int trig) // 초음파 센서 함수
 {
   int distance;
   digitalWrite(trig, HIGH);delay(2);
